8fd4da3b41159504d00f24081bd80e3a9033414f
aszellem <aszellemek@gmail.com>
2022-10-04 19:59:46 +0200

    doc

--- Exercise.java
+++ Exercise.java
@@ -7,14 +7,24 @@ public class Exercise {
         System.out.println(findPair(arr, k));
     }
 
+    /**
+     * Name: findPair
+     * 
+     * @param arr
+     * @param k
+     * @return boolean
+     * 
+     *         Inside: checks if there exist two elements in arr
+     *         such that we get k when we add them together.
+     */
     public static boolean findPair(int[] arr, int k) {
         for (int i = 0; i < arr.length; i++) {
-            for (int j = i+1; j < arr.length; j++) {
+            for (int j = i + 1; j < arr.length; j++) {
                 if ((arr[i] + arr[j]) == k) {
                     return true;
                 }
             }
         }
-        return false;        
+        return false;
     }
 }

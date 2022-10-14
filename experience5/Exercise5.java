5b95f1a2400d768ad51f32ac9026d4857f1e7a85
aszellem <aszellemek@gmail.com>
2022-10-14 16:54:52 +0200

    5

---
+++ experience5/Exercise5.java
@@ -0,0 +1,8 @@
+package experience5;
+public class Exercise5 {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ experience5/Tree.java
@@ -0,0 +1,31 @@
+package experience5;
+
+public class Tree {
+    int data;
+    Tree left;
+    Tree right;
+
+    Tree(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    Tree(int data, Tree left, Tree right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+
+    public void dfsPreorder(Tree root) {
+        // your code here
+    }
+
+    public void dfsInorder(Tree root) {
+        // your code here
+    }
+
+    public void dfsPostorder(Tree root) {
+        // your code here
+    }
+}

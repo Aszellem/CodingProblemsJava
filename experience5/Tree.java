16c456c4170bb3b8e2bd9ea25c9e99a94bc9eba3
aszellem <aszellemek@gmail.com>
2022-10-14 17:07:17 +0200

    done

--- experience5/Tree.java
+++ experience5/Tree.java
@@ -17,15 +17,75 @@ public class Tree {
         this.right = right;
     }
 
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Tree getLeft() {
+        return left;
+    }
+
+    public void setLeft(Tree left) {
+        this.left = left;
+    }
+
+    public Tree getRight() {
+        return right;
+    }
+
+    public void setRight(Tree right) {
+        this.right = right;
+    }
+
+    /**
+     * name: dfsPreorder
+     * 
+     * @param root
+     * 
+     *             Inside: print the root value, then print the left subtree, then
+     *             print the right subtree.
+     */
     public void dfsPreorder(Tree root) {
-        // your code here
+        if (root == null)
+            return;
+        System.out.println(root.data);
+        dfsPreorder(root.left);
+        dfsPreorder(root.right);
     }
 
+    /**
+     * name: dfsInorder
+     * 
+     * @param root
+     * 
+     *             Inside: print the left subtree, then print the root value, then
+     *             print the right subtree.
+     */
     public void dfsInorder(Tree root) {
-        // your code here
+        if (root == null)
+            return;
+        dfsInorder(root.left);
+        System.out.println(root.data);
+        dfsInorder(root.right);
     }
 
+    /**
+     * name: dfsPostorder
+     * 
+     * @param root
+     * 
+     *             Inside: print the left subtree, then print the right subtree,
+     *             then print the root value.
+     */
     public void dfsPostorder(Tree root) {
-        // your code here
+        if (root == null)
+            return;
+        dfsPostorder(root.left);
+        dfsPostorder(root.right);
+        System.out.println(root.data);
     }
 }
